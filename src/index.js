d32eabf8cbce1a7f38baf40e9824ee9b9802ace4
Mathieu Le Ray <leraymathieu@gmail.com>
2023-02-10 17:26:50 +0100

    Configuration et initialisation du projet

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import {log} from './js/controllers/main';
+
+//On récupère la balise title et on y ajoute du texte
+const title = document.querySelector('title').textContent = "Romie";
+
+log(300);
+
---
+++ src/js/controllers/main.js
@@ -0,0 +1,4 @@
+
+export let log = (status) => {
+    console.log(status);
+} 
